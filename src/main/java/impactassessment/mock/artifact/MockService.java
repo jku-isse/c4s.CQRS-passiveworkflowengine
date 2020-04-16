0d521fd8233539392ac2f537a5cb1641279c2868
Bahlok <steff396@gmail.com>
2020-04-16 12:55:36 +0200

    artifact mock

---
+++ src/main/java/impactassessment/mock/artifact/MockService.java
@@ -0,0 +1,14 @@
+package impactassessment.mock.artifact;
+
+import org.springframework.context.annotation.Bean;
+
+public class MockService {
+    public static Artifact mockArtifact(String id) {
+        Artifact a = new Artifact();
+        a.setField("id", id);
+        a.setField("status", "resolved");
+        a.setField("issuetype", "Hazard");
+        a.setField("priority", "high");
+        return a;
+    }
+}
