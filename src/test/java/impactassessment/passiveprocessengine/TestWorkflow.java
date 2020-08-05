ad71ec7c106472f91435c98b3dae94dada6a4644
stefanb <steff396@gmail.com>
2020-08-05 14:15:45 +0200

    #51 created test workflow

---
+++ src/test/java/impactassessment/passiveprocessengine/TestWorkflow.java
@@ -0,0 +1,67 @@
+package impactassessment.passiveprocessengine;
+
+import impactassessment.model.definition.ArtifactTypes;
+import impactassessment.model.workflowmodel.*;
+
+public class TestWorkflow extends AbstractWorkflowDefinition implements WorkflowDefinition {
+
+    public static final String WORKFLOW_TYPE = "TEST_WORKFLOW_TYPE";
+
+    public static final String TASK_STATE_OPEN = "Open";
+    public static final String TASK_STATE_CLOSED = "Closed";
+
+    public static final String INPUT_ROLE_WPTICKET = "INPUT_ROLE_WPTICKET";
+
+
+    public TestWorkflow() {
+        super(WORKFLOW_TYPE);
+    }
+
+    @Override
+    public WorkflowInstance createInstance(String id) {
+        initWorkflowSpecification();
+        return new WorkflowInstance(id, this, pub);
+    }
+
+    private void initWorkflowSpecification() {
+        // simple workflow:
+        // OPEN --> CLOSED
+        
+        TaskDefinition tdOpen = getStateOpenTaskDefinition();
+        taskDefinitions.add(tdOpen);
+        TaskDefinition tdClosed = getStateClosedTaskDefinition();
+        taskDefinitions.add(tdClosed);
+
+        dnds.add(getWfKickOff(tdOpen));
+        dnds.add(getOpen2Closed(tdOpen, tdClosed));
+    }
+
+    private TaskDefinition getStateOpenTaskDefinition() {
+        TaskDefinition td = new TaskDefinition(TASK_STATE_OPEN, this);
+        return td;
+    }
+
+    private TaskDefinition getStateClosedTaskDefinition() {
+        TaskDefinition td = new TaskDefinition(TASK_STATE_CLOSED, this);
+        td.getExpectedInput().put(INPUT_ROLE_WPTICKET, new ArtifactType(ArtifactTypes.ARTIFACT_TYPE_JIRA_TICKET));
+        return td;
+    }
+
+    private DecisionNodeDefinition getWfKickOff(TaskDefinition tdOpen) {
+        DecisionNodeDefinition dnd = new DecisionNodeDefinition(
+                "workflowKickOff",
+                this,
+                DecisionNodeDefinition.NO_EXTERNAL_RULE,
+                DecisionNodeDefinition.NO_EXTERNAL_RULE,
+                DecisionNodeDefinition.NO_EXTERNAL_RULE);
+        dnd.addOutBranchDefinition(new DefaultBranchDefinition("openIn", tdOpen, false, true, dnd));
+        return dnd;
+    }
+
+    private DecisionNodeDefinition getOpen2Closed(TaskDefinition tdOpen, TaskDefinition tdClosed) {
+        DecisionNodeDefinition dnd = new DecisionNodeDefinition("open2closed", this, DecisionNodeDefinition.HAVING_EXTERNAL_RULE, DecisionNodeDefinition.NO_EXTERNAL_RULE, DecisionNodeDefinition.NO_EXTERNAL_RULE);
+        dnd.addInBranchDefinition(new DefaultBranchDefinition("openOut", tdOpen, false, true, dnd));
+        dnd.addOutBranchDefinition(new DefaultBranchDefinition("closedIn", tdClosed, false, true, dnd));
+        return dnd;
+    }
+}
---
+++ src/test/java/impactassessment/passiveprocessengine/WorkflowTaskInputOutputMappingTest.java
@@ -0,0 +1,17 @@
+package impactassessment.passiveprocessengine;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WorkflowTaskInputOutputMappingTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
