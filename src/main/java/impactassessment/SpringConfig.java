23f0ee4e511a9f06876d69017248abd1dd346007
stefanb <steff396@gmail.com>
2020-08-25 13:56:50 +0200

    moved configurations to application.properties file

--- src/main/java/impactassessment/SpringConfig.java
+++ src/main/java/impactassessment/SpringConfig.java
@@ -18,22 +18,16 @@ public class SpringConfig {
 
     @Bean
     public IJiraArtifactService getJiraArtifactService() {
-        // connects directly to a Jira server. URI, username and password are defined in application.properties
+        // connects directly to a Jira server
 //        return new JiraService();
-        // uses JSON image of Jira data with the stated filename in resources folder
-        return new JiraJsonService("dronology_jira_big.json");
-    }
-
-    @Bean
-    @Scope("prototype")
-    public KieSessionWrapper getKieSessionWrapper(CommandGateway commandGateway, IJiraArtifactService artifactService) {
-        // define which rule files (have to be located in "/resources/rules/") should be taken to build up kieSessions
-        KieSession kieSession = new KieSessionFactory().getKieSession("execution_qac.drl", "constraints.drl");
-        return new KieSessionWrapper(commandGateway, artifactService, kieSession);
+        // uses JSON image of Jira data in resources folder
+        return new JiraJsonService();
     }
 
     @Bean
     public AbstractWorkflowDefinition getAbstractWorkflowDefinition() {
+        // careful! execution.drl has hardcoded (dronology)workflow-branch names
+        // --> just injecting a new workflow here won't work
         return new DronologyWorkflow();
     }
 
