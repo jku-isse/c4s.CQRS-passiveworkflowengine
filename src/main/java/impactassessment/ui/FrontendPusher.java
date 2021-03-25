c05176961aaf3aabc0ab559aadab5e5e2fd49969
stefanb <steff396@gmail.com>
2021-03-25 12:44:52 +0100

    implemented possibility to decrease frontend updates (turned off)

--- src/main/java/impactassessment/ui/FrontendPusher.java
+++ src/main/java/impactassessment/ui/FrontendPusher.java
@@ -4,25 +4,36 @@ import com.vaadin.flow.component.UI;
 import impactassessment.passiveprocessengine.WorkflowInstanceWrapper;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 @Component
 @Scope("singleton")
 @NoArgsConstructor
+@Slf4j
 public class FrontendPusher implements IFrontendPusher {
 
     private @Setter UI ui;
     private @Setter MainView view;
 
+    private Instant lastUpdate = Instant.now();
+    private final boolean PERFORMANCE_MODE = false;
+
     @Override
     public void update(List<WorkflowInstanceWrapper> state) {
-        if (ui != null && view != null) {
-            ui.access(() -> view.getGrids().stream()
-                    .filter(com.vaadin.flow.component.Component::isVisible)
-                    .forEach(grid -> grid.updateTreeGrid(state)));
+        if (!PERFORMANCE_MODE || Duration.between(lastUpdate, Instant.now()).getNano() > 200000000) {
+            log.debug("update frontend");
+            lastUpdate = Instant.now();
+            if (ui != null && view != null) {
+                ui.access(() -> view.getGrids().stream()
+                        .filter(com.vaadin.flow.component.Component::isVisible)
+                        .forEach(grid -> grid.updateTreeGrid(state)));
+            }
         }
     }
 
