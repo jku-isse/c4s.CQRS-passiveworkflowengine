b023623aeb0f9f514fd99b888c780bc16ee338c3
Christoph Steidl <christoph.steidl@outlook.de>
2021-07-26 17:17:28 +0200

    Fixed some dependency problems in pom.xml and other files.

--- src/main/java/impactassessment/artifactconnector/demo/DemoRequirement.java
+++ src/main/java/impactassessment/artifactconnector/demo/DemoRequirement.java
@@ -5,16 +5,27 @@ import artifactapi.ArtifactType;
 
 public class DemoRequirement extends DemoArtifact {
 
-	public enum propKeys { status, assessment, type };
-	public enum statusValues { open, inprogress, completed };
-	public enum assessmentValues { na , inpreparation, done };
-	public enum typeValues { highlevel, lowlevel };
-	
+	public enum propKeys {
+		status, assessment, type
+	};
+
+	public enum statusValues {
+		open, inprogress, completed
+	};
+
+	public enum assessmentValues {
+		na, inpreparation, done
+	};
+
+	public enum typeValues {
+		highlevel, lowlevel
+	};
+
 	public DemoRequirement(DemoService ds, String id) {
 		super(ds, id);
 		this.ai = new ArtifactIdentifier(id, type.getId());
-		this.properties.put(propKeys.status.toString() , statusValues.open.toString());
-		this.properties.put(propKeys.assessment.toString() , assessmentValues.na.toString());
+		this.properties.put(propKeys.status.toString(), statusValues.open.toString());
+		this.properties.put(propKeys.assessment.toString(), assessmentValues.na.toString());
 		this.properties.put(propKeys.type.toString(), typeValues.lowlevel.toString());
 	}
 
