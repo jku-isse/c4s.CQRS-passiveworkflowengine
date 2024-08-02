99f02051d454d01a327013a706d9118e82672835
Christoph Mayr-Dorn <christoph.mayr-dorn@jku.at>
2024-08-02 11:56:17 +0200

    enabled non-authenticated access to processrepair endpoint

--- src/main/java/at/jku/isse/passiveprocessengine/frontend/security/SecurityConfig.java
+++ src/main/java/at/jku/isse/passiveprocessengine/frontend/security/SecurityConfig.java
@@ -154,6 +154,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	"/connectors/**",
     	"/constraint/**",
     	"/process/**",
+    	"/processrepairs/**",
     	// Client-side JS
         "/VAADIN/**",
 
